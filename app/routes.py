2b42dc03211f9c050ffa78a557b777b465560df8
JebinDanielR <jebinderil@gmail.com>
2026-07-21 20:36:23 +0530

    Implemented CRUD endpoints for expenses with filtering

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
-from app.schemas import CategoryCreate, CategoryResponse, CategoryUpdate
+from app.schemas import CategoryCreate, CategoryResponse, CategoryUpdate, ExpenseCreate, ExpenseResponse, ExpenseUpdate
 from app.database import get_db
-from app.models import Category
+from app.models import Category,Expense
 from sqlalchemy.orm import Session
+from sqlalchemy import and_
 from fastapi import HTTPException, status, Response, Depends
+from datetime import date
 
 router = APIRouter()
 
@@ -15,19 +17,12 @@ def home():
     }
 
 # GET /categories
-@router.get(
-    "/categories",
-    response_model=list[CategoryResponse]
-)
+@router.get("/categories", response_model=list[CategoryResponse])
 def get_categories(db: Session = Depends(get_db)):
     return db.query(Category).all()
 
-
 # GET /categories/{id}
-@router.get(
-    "/categories/{id}",
-    response_model=CategoryResponse
-)
+@router.get("/categories/{id}",response_model=CategoryResponse)
 def get_category(id: int, db: Session = Depends(get_db)):
     category = db.get(Category, id)
 
@@ -39,13 +34,8 @@ def get_category(id: int, db: Session = Depends(get_db)):
 
     return category
 
-
 # POST /categories
-@router.post(
-    "/categories",
-    response_model=CategoryResponse,
-    status_code=status.HTTP_201_CREATED
-)
+@router.post("/categories", response_model=CategoryResponse, status_code=status.HTTP_201_CREATED)
 def create_category(category: CategoryCreate, db: Session = Depends(get_db)):
     existing = (
         db.query(Category)
@@ -67,12 +57,8 @@ def create_category(category: CategoryCreate, db: Session = Depends(get_db)):
 
     return new_category
 
-
 # PUT /categories/{id}
-@router.put(
-    "/categories/{id}",
-    response_model=CategoryResponse
-)
+@router.put("/categories/{id}", response_model=CategoryResponse)
 def update_category(
     id: int,
     category_data: CategoryUpdate,
@@ -108,12 +94,8 @@ def update_category(
 
     return category
 
-
 # DELETE /categories/{id}
-@router.delete(
-    "/categories/{id}",
-    status_code=status.HTTP_204_NO_CONTENT
-)
+@router.delete("/categories/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_category(id: int, db: Session = Depends(get_db)):
     category = db.get(Category, id)
 
@@ -133,3 +115,144 @@ def delete_category(id: int, db: Session = Depends(get_db)):
     db.commit()
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+def expense_response(expense):
+    return {
+        "id": expense.id,
+        "amount": expense.amount,
+        "description": expense.description,
+        "spent_on": expense.spent_on,
+        "category_id": expense.category_id,
+        "category_name": expense.category.name
+    }
+
+@router.get("/expenses", response_model=list[ExpenseResponse])
+def get_expenses(
+    category_id: int | None = None,
+    from_date: date | None = None,
+    to_date: date | None = None,
+    db: Session = Depends(get_db)
+):
+
+    query = db.query(Expense)
+
+    if category_id:
+        query = query.filter(
+            Expense.category_id == category_id
+        )
+
+    if from_date:
+        query = query.filter(
+            Expense.spent_on >= from_date
+        )
+
+    if to_date:
+        query = query.filter(
+            Expense.spent_on <= to_date
+        )
+
+    expenses = query.all()
+
+    return [
+        expense_response(expense)
+        for expense in expenses
+    ]
+
+@router.get("/expenses/{id}", response_model=ExpenseResponse)
+def get_expense(
+    id:int,
+    db:Session=Depends(get_db)
+):
+
+    expense = db.get(Expense,id)
+
+    if expense is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Expense not found"
+        )
+
+    return expense_response(expense)
+
+@router.post("/expenses", response_model=ExpenseResponse, status_code=201)
+def create_expense(
+    expense_data: ExpenseCreate,
+    db:Session=Depends(get_db)
+):
+
+    category = db.get(
+        Category,
+        expense_data.category_id
+    )
+
+    if category is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Category not found"
+        )
+
+    expense = Expense(
+        amount=expense_data.amount,
+        description=expense_data.description,
+        spent_on=expense_data.spent_on,
+        category_id=expense_data.category_id
+    )
+
+    db.add(expense)
+    db.commit()
+    db.refresh(expense)
+
+    return expense_response(expense)
+
+@router.put("/expenses/{id}", response_model=ExpenseResponse)
+def update_expense(
+    id:int,
+    expense_data:ExpenseUpdate,
+    db:Session=Depends(get_db)
+):
+
+    expense=db.get(Expense,id)
+
+    if expense is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Expense not found"
+        )
+
+    category=db.get(
+        Category,
+        expense_data.category_id
+    )
+
+    if category is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Category not found"
+        )
+
+    expense.amount=expense_data.amount
+    expense.description=expense_data.description
+    expense.spent_on=expense_data.spent_on
+    expense.category_id=expense_data.category_id
+
+    db.commit()
+    db.refresh(expense)
+
+    return expense_response(expense)
+
+@router.delete("/expenses/{id}", status_code=204)
+def delete_expense(
+    id:int,
+    db:Session=Depends(get_db)
+):
+
+    expense=db.get(Expense,id)
+
+    if expense is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Expense not found"
+        )
+
+    db.delete(expense)
+    db.commit()
--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from datetime import date
 
 
 class CategoryCreate(BaseModel):
@@ -12,6 +13,32 @@ class CategoryUpdate(BaseModel):
 class CategoryResponse(BaseModel):
     id: int
     name: str
+
+    model_config = {
+        "from_attributes": True
+    }
+
+class ExpenseCreate(BaseModel):
+    amount: float = Field(gt=0)
+    description: str | None = None
+    spent_on: date
+    category_id: int
+
+
+class ExpenseUpdate(BaseModel):
+    amount: float = Field(gt=0)
+    description: str | None = None
+    spent_on: date
+    category_id: int
+
+
+class ExpenseResponse(BaseModel):
+    id: int
+    amount: float
+    description: str | None
+    spent_on: date
+    category_id: int
+    category_name: str
 
     model_config = {
         "from_attributes": True
